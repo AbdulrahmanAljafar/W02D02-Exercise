0e73212b588599cd625fadb04c1d00ae7ce377b1
Abdulrahman_Aljafar <abdulrahmana.jafar@gmail.com>
2021-04-19 23:51:43 +0300

    My work in secand exercise

--- abstract_class/abstract_class/Program.cs
+++ abstract_class/abstract_class/Program.cs
@@ -70,7 +70,7 @@ namespace abstract_class
             
         }
 
-         public class NumberTokenizer  : Tokenizable
+         public class IntOrfloatTokenizer : Tokenizable
          {
             public override bool tokenizable(Tokenizer t)
             {
@@ -79,19 +79,42 @@ namespace abstract_class
              public override Token tokenize(Tokenizer t)
              {
                 Token token = new Token();
+                int count = 0;
+                int currentpos = 0;
                 token.value = "";
-                token.type = "number";
+                token.type = "int";
                 token.poition = t.currentPosition;
                 token.lineNumber = t.lineNumber;
 
-                while (t.hasMore() && char.IsDigit(t.peek()))
+                while (t.hasMore() && (char.IsDigit(t.peek())|| t.peek()=='.' || t.peek() == 'f' ) )
                 {
-                    token.value += t.next();
+                    if(t.peek() == '.' || t.peek() == 'f')
+                    {
+                        token.type = "float";
+                        count++;
+                        token.value += t.next();
+                    }
+                    if (t.peek() == 'f')
+                    {
+                        if(t.peek(2) != ' ')
+                        {
+                            return null;
+                        }
+                    }
+                    if (count < 2)
+                    {
+                        token.value += t.next();
+                        
+
+                    }
+                   
                 }
                 return token;
              }
         }
 
+
+        //22.22
         public class WhiteSpace : Tokenizable
         {
             public override bool tokenizable(Tokenizer t)
@@ -118,7 +141,7 @@ namespace abstract_class
         {
             public override bool tokenizable(Tokenizer t)
             {
-                return t.hasMore() && Char.IsLetterOrDigit(t.peek()) || t.peek() == '_';
+                return (t.hasMore() && (Char.IsLetter(t.peek()) || t.peek() == '_'));
             }
             public override Token tokenize(Tokenizer t)
             {
@@ -128,22 +151,121 @@ namespace abstract_class
                 token.poition = t.currentPosition;
                 token.lineNumber = t.lineNumber;
 
-                while (t.hasMore() && Char.IsLetterOrDigit(t.peek()))
+                while (t.hasMore() && (Char.IsLetterOrDigit(t.peek()) || t.peek() == '_'))
                 {
+                    
                     token.value += t.next();
                 }
+                
                 return token;
             }
         }
+
+        public class hachTokenizable : Tokenizable
+        {
+            public override bool tokenizable(Tokenizer t)
+            {
+                return (t.hasMore() && t.peek() == '#');
+            }
+            public override Token tokenize(Tokenizer t)
+            {
+                Token token = new Token();
+                token.value = "";
+                token.type = "Hach";
+                token.poition = t.currentPosition;
+                token.lineNumber = t.lineNumber;
+
+                token.value += t.next();
+                while (t.hasMore() && (Char.IsLetterOrDigit(t.peek())))
+                {
+                    token.value += t.next();
+                }
+
+                return token;
+            }
+        }
+
+        public class userTokenizable : Tokenizable
+        {
+            public override bool tokenizable(Tokenizer t)
+            {
+                return (t.hasMore() && t.peek() == '@');
+            }
+            public override Token tokenize(Tokenizer t)
+            {
+                Token token = new Token();
+                token.value = "";
+                token.type = "user";
+                token.poition = t.currentPosition;
+                token.lineNumber = t.lineNumber;
+
+                token.value += t.next();
+                while (t.hasMore() && (Char.IsLetterOrDigit(t.peek())))
+                {
+                    token.value += t.next();
+                }
+
+                return token;
+            }
+        }
+
+        public class xmlTokenizable : Tokenizable
+        {
+            public override bool tokenizable(Tokenizer t)
+            {
+                return (t.hasMore() && t.peek() == '<');
+            }
+            public override Token tokenize(Tokenizer t)
+            {
+                Token token = new Token();
+                token.value = "";
+                token.type = "open tag xml";
+                token.poition = t.currentPosition;
+                token.lineNumber = t.lineNumber;
+
+                token.value += t.next();
+                while (t.hasMore() && (Char.IsLetterOrDigit(t.peek()) || t.peek() == '>' || t.peek() == '/' ))
+                {
+                    if (t.peek() == '/')
+                    {
+                        if (token.value == "<")
+                        {
+                            token.type = "close tag xml";
+                        }
+                        else
+                        {
+                            
+                            return null;
+                        }
+                     
+
+                    }
+                    token.value += t.next();
+                    
+                    if (t.peek() == '>')
+                    {
+                        token.value += t.next();
+                        break;
+                    }
+                }
+
+
+                return token;
+            }
+        }
+
+
+        
+
         static void Main(string[] args)
         {
-            Tokenizer t = new Tokenizer("1234 12if  jdcnkjd");
-            Tokenizable[] handers = new Tokenizable[] { new NumberTokenizer(),new WhiteSpace(),new idTokenizer() };
+            Tokenizer t = new Tokenizer("#vjdnkd 20 20.12f 19 if  _jdc_nkjd  20f @Abdulrahman <to> hhh  </to> jjj ");
+            Tokenizable[] handers = new Tokenizable[] { new xmlTokenizable(),new IntOrfloatTokenizer(),new WhiteSpace(),new idTokenizer(),new hachTokenizable(),new userTokenizable() };
             Token token = t.tokenize(handers);
             while(token != null)
             {
 
-                Console.WriteLine(token.value);
+                Console.WriteLine(token.value+" | "+token.type);
                 token = t.tokenize(handers);
             }
             
